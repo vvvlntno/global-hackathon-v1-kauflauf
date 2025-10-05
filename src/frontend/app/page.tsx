6da66483289a98c4fb2867af249ca6a70bc19536
vvvlntno <vvvlntno@gmail.com>
2025-10-05 10:39:18 +0200

    dev: changed background to snapshot

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
     <main className="relative flex h-screen w-full overflow-hidden bg-black">
       {/* Linke Seite */}
       <motion.div
-        className="absolute inset-0 flex items-start justify-start pl-16 pt-16 bg-[url('/store.jpg')] bg-cover bg-center text-white"
+          className="absolute inset-0 flex items-start justify-start pl-16 pt-16 bg-[url('https://kwgiklgjqpkkpitzappr.supabase.co/storage/v1/object/public/background-image/bg_cropped2.png')] bg-cover bg-center text-white"
         style={{
           clipPath: "polygon(0 0, 25% 0, 95% 100%, 0% 100%)",
         }}
